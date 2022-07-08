8c26f2ea5e6789028d44c76605e613d7243e122b
miguelvalden <miguelvalden77@gmail.com>
2022-07-08 12:58:21 +0200

    Recursividad y limpieza del canvas

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,35 @@
 //Variables globales
 const canvas = document.getElementById("canvas")
-const contexto = document.getContext("2d")
+const contexto = canvas.getContext("2d")
 const startScreen = document.getElementById("start")
 const startButton = document.getElementById("start-button")
+const FPS = 20
 
 
+
+// Funciones
+const inicio = ()=>{
+    gameLoop()
+}
+
+const gameLoop = ()=>{
+    console.log("Funciono")
+
+    // Limpieza del canvas
+    contexto.clearRect(0, 0, canvas.width, canvas.height)
+
+    // Movimientos
+
+
+    // Dibujados
+
+
+    // Recursión
+    setInterval(gameLoop, 1000 / FPS)
+
+}
+
+
+// AddEventListeners
+addEventListener("load", inicio)
 

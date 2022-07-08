88c1b1dd46b9819fbd09fc575677a17345a4de48
miguelvalden <miguelvalden77@gmail.com>
2022-07-08 12:25:58 +0200

    Primeros pasos en el proyecto

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+//Variables globales
+const canvas = document.getElementById("canvas")
+const contexto = document.getContext("2d")
+const startScreen = document.getElementById("start")
+const startButton = document.getElementById("start-button")
+
+
+

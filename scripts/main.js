0b85c35c487eafe8615f0ca09f6ddbf8068d3bae
miguelvalden <miguelvalden77@gmail.com>
2022-07-10 20:55:34 +0200

    Reestructuración de las clases para que haya más variables globales

--- scripts/main.js
+++ scripts/main.js
@@ -1,63 +1,325 @@
-//Variables globales
 const canvas = document.getElementById("canvas")
 const contexto = canvas.getContext("2d")
+const FPS = 100
+const startBtn = document.getElementById("start-button")
 const startScreen = document.getElementById("start")
-const startButton = document.getElementById("start-button")
-const FPS = 50
-const tablero = new Tablero()
+
+const piezas = [
+    [
+      [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 0, 0],
+        [0, 1, 1, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0]
+      ]
+    ],
+  
+    [
+      [
+        [0, 0, 0, 0],
+        [2, 2, 2, 2],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 2, 0],
+        [0, 0, 2, 0],
+        [0, 0, 2, 0],
+        [0, 0, 2, 0]
+      ],
+  
+      [
+        [0, 0, 0, 0],
+        [2, 2, 2, 2],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 2, 0],
+        [0, 0, 2, 0],
+        [0, 0, 2, 0],
+        [0, 0, 2, 0]
+      ]
+    ],
+  
+    [
+      [
+        [0, 0, 0, 0],
+        [0, 0, 3, 3],
+        [0, 3, 3, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 3, 0],
+        [0, 0, 3, 3],
+        [0, 0, 0, 3],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 0, 0],
+        [0, 0, 3, 3],
+        [0, 3, 3, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 3, 0],
+        [0, 0, 3, 3],
+        [0, 0, 0, 3],
+        [0, 0, 0, 0]
+      ]
+    ],
+  
+    [
+      [
+        [0, 0, 0, 0],
+        [0, 4, 4, 0],
+        [0, 0, 4, 4],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 0, 4],
+        [0, 0, 4, 4],
+        [0, 0, 4, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 0, 0],
+        [0, 4, 4, 0],
+        [0, 0, 4, 4],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 0, 4],
+        [0, 0, 4, 4],
+        [0, 0, 4, 0],
+        [0, 0, 0, 0]
+      ]
+    ],
+  
+    [
+      [
+        [0, 0, 0, 0],
+        [0, 5, 5, 5],
+        [0, 5, 0, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 5, 0],
+        [0, 0, 5, 0],
+        [0, 0, 5, 5],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 0, 5],
+        [0, 5, 5, 5],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 5, 5, 0],
+        [0, 0, 5, 0],
+        [0, 0, 5, 0],
+        [0, 0, 0, 0]
+      ]
+    ],
+  
+    [
+      [
+        [0, 0, 0, 0],
+        [0, 6, 6, 6],
+        [0, 0, 0, 6],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 6, 6],
+        [0, 0, 6, 0],
+        [0, 0, 6, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 6, 0, 0],
+        [0, 6, 6, 6],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 6, 0],
+        [0, 0, 6, 0],
+        [0, 6, 6, 0],
+        [0, 0, 0, 0]
+      ]
+    ],
+  
+    [
+      [
+        [0, 0, 0, 0],
+        [0, 7, 7, 7],
+        [0, 0, 7, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 7, 0],
+        [0, 0, 7, 7],
+        [0, 0, 7, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 7, 0],
+        [0, 7, 7, 7],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+  
+      [
+        [0, 0, 7, 0],
+        [0, 7, 7, 0],
+        [0, 0, 7, 0],
+        [0, 0, 0, 0]
+      ]
+    ]
+]
+const tablero = [
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9],
+    [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9]
+]
+
+const anchoPieza = 40
+const altoPieza = 40
+let pieza
+const colores = ["red", "green", "blue", "brown", "orange", "pink", "purple"]
+
+// Función creadora
+const ObjetoPieza = function(){
+
+    this.x = 0
+    this.y = 0
+    this.angulo = 1
+    this.tipoPieza = 1
+
+    this.dibujar = function(){
+        for(fila = 0; fila < 4; fila ++){
+            for(col = 0; col < 4; col ++){
+                if(piezas[this.tipoPieza][this.angulo][fila][col] !== 0){
+                    contexto.fillStyle = colores[this.tipoPieza]
+                    const x0 = (this.x + col) * anchoPieza
+                    const y0 = (this.y + fila) * altoPieza
+                    contexto.fillRect(x0, y0, anchoPieza, altoPieza)
+                }
+            }
+        }
+    }
+}
 
 
 // Funciones
+const dibujarTablero = ()=>{
+    for(let fila = 0; fila < 16; fila ++){
+        for(let col = 1; col < 10; col ++){
+            if(tablero[fila][col] !== 0){
+                contexto.fillStyle = colores[tablero[fila][col]]
+                const x0 = col * anchoPieza
+                const y0 = fila * altoPieza
+                contexto.fillRect(x0, y0, anchoPieza, altoPieza)
+            }
+        }
+    }
+}
+
+const listeners = ()=>{
+    addEventListener("keydown", evt =>{
+        const {key} = evt
+        switch(key){
+            case "ArrowUp": 
+            break
+
+            case "ArrowDown": 
+            break
+
+            case "ArrowLeft": 
+            break
+
+            case "ArrowRight": 
+            break
+        }
+    })
+}
 
 const inicio = ()=>{
 
     listeners()
+    pieza = new ObjetoPieza()
 
     setInterval(gameLoop, 1000 / FPS)
-    
+
 }
 
 const gameLoop = ()=>{
-    console.log("Funciono")
 
-    // Limpieza del canvas
-    tablero.pieza.limpiarCanvas()
+    dibujarTablero()
+    pieza.dibujar()
 
-    // Movimientos
-    tablero.pieza.estaAbajo()
-
-    // Dibujados
-    tablero.dibujarPanel()
-    tablero.pieza.dibujarPiezas()
 }
 
 
-// AddEventListeners
-startButton.addEventListener("click", ()=>{
 
+// AddEventListeners
+startBtn.addEventListener("click", ()=>{
     startScreen.style.display = "none"
     canvas.style.display = "block"
 
     addEventListener("load", inicio())
 
 })
-
-const listeners = ()=>{
-    addEventListener("keydown", evt =>{
-        const {key} = evt
-        switch(key){
-            case "ArrowUp": tablero.pieza.rotar()
-            break
-
-            case "ArrowDown": 
-            break
-
-            case "ArrowLeft": tablero.pieza.moveIzq()
-            break
-
-            case "ArrowRight": tablero.pieza.moveDrch()
-            break
-        }
-    })
-}
 
